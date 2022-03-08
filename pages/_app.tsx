e9975fd578b08ef71f2b299d0bddd6591d707351
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-08 22:46:12 +0530

    new funciotn added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { SessionProvider } from "next-auth/react";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider
+
+      session={pageProps.session}
+    >
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
-
-export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,99 +1,48 @@
 /* eslint-disable @next/next/link-passhref */
 
-
 import Link from "next/link";
 import * as React from "react";
+import {
+  getSession,
+  GetSessionParams,
+  signIn,
+  signOut,
+  useSession,
+} from "next-auth/react";
+import { GetServerSideProps } from "next";
+import { Session } from "next-auth";
 
 export default function Home() {
-  const data = [
-    {
-      name: "Fashion",
-    },
-    {
-      name: "Shooting",
-    },
-    {
-      name: "Creativity",
-    },
-    {
-      name: "Darkside",
-    },
-    {
-      name: "Art",
-    },
-    {
-      name: "Wallpapers",
-    },
-    {
-      name: "HD",
-    },
-  ];
+  const { data: session, status } = useSession();
 
+
+  if (status === "authenticated") {
+    return <div className="bg-black min-h-screen text-white"><p>Signed in as {session.user.ema il}</p></div> 
+  }
   return (
     <div className="flex flex-col bg-black container text-white min-h-screen">
-      <style jsx>{`
-        .container {
-          background-repeat: no-repeat;
-          background-size: 100%;
-          opacity: 10;
-          background-image: url("https://images.unsplash.com/photo-1625417026759-69162d7ded55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80");
-          background-color: black;
-          position: fixed;
-          top: 0;
-          overflow: scroll;
-        }
-      `}</style>
-      <div className="bg-black/30">
-        <div className="flex justify-center items-center">
-          <div>
-            <img
-              src="https://source.unsplash.com/120x120/?portrait"
-              className="rounded-full h-[3.4rem] mt-3"
-              alt="Avatar"
-            />
-          </div>
-          <h1 className="font-bold text-4xl mt-3 p-4 text-center">
-            {" "}
-            @sour_moha{" "}
-          </h1>
-        </div>
-        {/* <div className="">
-        <img
-        className="h-full opacity-60 "
-        src="https://images.unsplash.com/photo-1625417026759-69162d7ded55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80"
-        />{" "}
-      </div> */}
-
-        <div className="rounded-t-[3rem] backdrop-blur-xl   bg-slate-700/30 h-[40rem] relative  top-[15rem] ">
-          <div className="p-5 flex flex-col mt-4">
-            <div>
-              <h1 className="text-2xl font-bold ">About</h1>
-              <p className="text-gray-300">
-                Photography It s my hoby and my love and before I knew it I ve
-                realized that photography is my true passion . I find myself in.
-                creative portraits, darkside and dimensions, everything now
-                draws me to a deep feeling of connection to art.
-              </p>
-            </div>
-            <div className="flex flex-col">
-              <h1 className="text-2xl font-bold">Interests</h1>
-              <div className="flex flex-wrap">
-                {data.map((n, index) => {
-                  return (
-                    <button
-                      key={index}
-                      className="bg-slate-600 mx-2 my-1 py-2 px-4 text-lg rounded-lg"
-                    >
-                      {n.name}
-                    </button>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as {session.user.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
     </div>
   );
 }
 //Shooting Creativity Darkside HD Art Wallpapers
+export const getServerSideProps: GetServerSideProps<{
+  session: Session | null;
+}> = async (context) => {
+  return {
+    props: {
+      session: await getSession(context),
+    },
+  };
+};
