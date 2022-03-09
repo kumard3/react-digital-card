c01c90cd33b1ee4ed066f2cb4fe40c54f6918543
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-10 02:31:58 +0530

    test

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,10 @@
 import NextAuth from "next-auth";
 
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
 
 export default NextAuth({
-  // adapter: PrismaAdapter(prisma),
+
   providers: [
 
     GoogleProvider({
